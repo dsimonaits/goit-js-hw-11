de9c7041f7e218554d53bc767d0e151a34611c9a
Deniss Simonaits <dsimonaits@gmail.com>
2022-10-24 22:38:44 +0300

    axios update

--- src/js/fetch-pixabay.js
+++ src/js/fetch-pixabay.js
@@ -1,45 +1,69 @@
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
-export class FetchPixabay {
-  static URL = 'https://pixabay.com/api/';
-  static #API = '30781043-697065eb175f1ee93924e4241';
+export { fetchPixabay };
 
-  constructor() {
-    this.searchQuery = '';
-    this.page = 1;
-  }
+const KEY = '30781043-697065eb175f1ee93924e4241';
 
-  async fetch() {
-    const params = new URLSearchParams({
-      key: FetchPixabay.#API,
-      q: this.searchQuery,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-      page: this.page,
-      per_page: 40,
+async function fetchPixabay(query, page, perPage) {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+        page: page,
+        per_page: perPage,
+      },
     });
-
-    const result = await fetch(`${FetchPixabay.URL}?${params}`);
-
-    return result.ok
-      ? result.json()
-      : Promise.reject(Notiflix.Notify.failure('Something went wrong'));
-  }
-
-  resetPage() {
-    this.page = 1;
-  }
-
-  incrementPage() {
-    this.page += 1;
-  }
-
-  get query() {
-    return this.searchQuery;
-  }
-
-  set query(value) {
-    this.searchQuery = value;
+    return response;
+  } catch (error) {
+    Notiflix.Notify.failure('Something went wrong');
   }
 }
+
+// export class FetchPixabay {
+//   static URL = 'https://pixabay.com/api/';
+//   static #API = '30781043-697065eb175f1ee93924e4241';
+
+//   constructor() {
+//     this.searchQuery = '';
+//     this.page = 1;
+//   }
+
+//   async fetch() {
+//     const params = new URLSearchParams({
+//       key: FetchPixabay.#API,
+//       q: this.searchQuery,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: 'true',
+//       page: this.page,
+//       per_page: 40,
+//     });
+
+//     const result = await fetch(`${FetchPixabay.URL}?${params}`);
+
+//     return result.ok
+//       ? result.json()
+//       : Promise.reject(Notiflix.Notify.failure('Something went wrong'));
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   get query() {
+//     return this.searchQuery;
+//   }
+
+//   set query(value) {
+//     this.searchQuery = value;
+//   }
+// }
