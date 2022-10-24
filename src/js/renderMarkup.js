093c8fa63ce849b1f59803bbf782d4ec96d3b626
Deniss Simonaits <dsimonaits@gmail.com>
2022-10-24 22:39:09 +0300

    Create renderMarkup.js

---
+++ src/js/renderMarkup.js
@@ -0,0 +1,42 @@
+export { renderMarkup };
+
+function renderMarkup(images, querySelector) {
+  const imagesMarkup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<a class="gallery-item" href="${largeImageURL}"><div class="photo-card">
+    <img class='gallery-image' src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+      <p class="info-item">
+        <b>Likes:</b> ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views:</b> ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments:</b> ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads:</b> ${downloads}
+      </p>
+    </div>
+  </div>
+  </a>`;
+      }
+    )
+    .join('');
+  querySelector.insertAdjacentHTML('beforeend', imagesMarkup);
+
+  lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  }).refresh();
+}
